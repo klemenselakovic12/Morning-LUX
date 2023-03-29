57cd301ba40eb057f0c258f3d5fba27c0b9a2b3c
Klemen Selakovic <klemen.selakovic@gmail.com>
2023-03-29 07:28:45 +0200

    Initial Commit

---
+++ MorningLux/MorningLuxApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorningLuxApp.swift
+//  MorningLux
+//
+//  Created by Klemen Selakovic on 29/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MorningLuxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MorningLuxUITests/MorningLuxUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MorningLuxUITestsLaunchTests.swift
+//  MorningLuxUITests
+//
+//  Created by Klemen Selakovic on 29/03/2023.
+//
+
+import XCTest
+
+final class MorningLuxUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
